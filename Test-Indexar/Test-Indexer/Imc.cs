3c31eb1f0d4a4b62cafec8a8257c543ef8d15af1
Maflante <rodrigoescala20@gmail.com>
2023-05-08 17:44:44 -0400

    Test de Indexar

---
+++ Test-Indexar/Test-Indexer/Imc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Indexer
+{
+    public class Datos
+    {
+        private int peso;
+        private double altura;
+        private string? clasificacion;
+        private double? imc;
+
+        public int Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
+        public string Clasificacion { get => clasificacion; set => clasificacion = value; }
+        public double? Imc { get => imc; set => imc = value; }
+
+        public void CalculoImc(int _Peso, double _Altura, string _Clasificacion ,
+            double _Imc)
+        {
+            Peso = _Peso;
+            Altura = _Altura;
+            clasificacion = _Clasificacion;
+            imc = _Imc;
+           
+            imc = Peso / (Altura*Altura);
+            
+            if (imc < 18)
+            {
+
+                Clasificacion = "Peso bajo. Necesario valorar signos de desnutrición.";
+            }
+            else if (imc > 18 && imc <= 24.9)
+            {
+                Clasificacion = "Normal";
+
+            }
+            else if (imc >= 27 && imc <= 29.9)
+            {
+                Clasificacion = "Obesidad grado I. Riesgo relativo ALTO para desarrolar enfermedades cardiovasculares.";
+            }
+            else if (imc >= 30 && imc <= 39.9)
+            {
+                Clasificacion = "Obesidad grado II. Riesgo relativo MUY ALTO para el desarrollo de enfermedades cardiovasculares.";
+
+            }
+            else if (imc >= 40)
+            {
+                Clasificacion = "Obesidad grado III. EXTREMA O MORBIDA. Riesgo relativo EXAGERADAMENTE ALTO para el desarrollo de enfermedades cardiovasculares";
+            }
+
+
+        }
+    }
+
+}
---
+++ Test-Indexar/Test-Indexer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Test_Indexer;
+
+//MASAS CORPORAL
+
+Console.WriteLine("Ingrese su peso");
+int Peso = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese su Altura");
+double Altura = Convert.ToDouble(Console.ReadLine());
+
+Datos CalculoImc = new Datos();
+
+CalculoImc.CalculoImc(Peso, Altura,"",0);
+
+Console.WriteLine("Su Imc es: " + CalculoImc.Imc + " " + CalculoImc.Clasificacion);
+
+
